8d90f0b073af399f6a8842a07c1ac78a8fe7b671
Bao Tran <77811454+kbreaker1314@users.noreply.github.com>
2021-01-24 09:37:38 -0800

    Add project files.

---
+++ CSC205Week01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSC205Week01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printing out the names of my teammates and me
+            Console.WriteLine("My teammates are: Evan Ritter, Yauchin Lam, Bao Tran (myself)");
+
+            //Caret
+            int count = 3;
+            int count2 = 3;
+            //first for loop is to print out the rows of the pattern
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = count; j > 0; j--)
+                {
+                    Console.Write(" ");                       
+                }
+
+                for (int k = count; k <= count2; k++)
+                {
+                    Console.Write("^");
+                }
+                count--;
+                count2++;
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
